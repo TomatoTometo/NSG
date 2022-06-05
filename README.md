ea7ffaa1af95c4ae4dfa989dd795e02d08a08779
Verde <Verde@knowhere>
2022-06-05 13:36:47 -0400

    Clean up

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ NSG (or Not So Great) is a collection of code that I write to learn algorithms/i
 This is all done for learning purposes, and thus they are "not so great"
 
 I welcome any suggestions/improvements!
+
+
+## Extra notes
+
+### MinGW
+
+Version GCC-8.1.0
+
+MinGW acquired from `https://www.mingw-w64.org/`
+
+Better downloaded from `https://sourceforge.net/projects/mingw-w64/files/`
+
+### Gtest
+Gtest version used on this project is v1.11.0 (`https://github.com/google/googletest/releases/tag/release-1.11.0`)
+
+Built and installed by the following commands
+```bash
+mkdir -p build/release
+cmake -B build/release/ -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake --build build/release/
+cmake --install build/release/
+```
+
+
--- test/circular-queue/gtest_circular_queue.cpp
+++ test/circular-queue/gtest_circular_queue.cpp
@@ -2,30 +2,40 @@
 
 #include "nsg/circular-queue/circular_queue.hpp"
 
-class CircularQueueTest : public ::testing::Test
+TEST(NSGCircularQueueTest, AllTestInOne)
 {
-public:
-  
-  void SetUp() override 
+  nsg::CircularQueue<int, 10> queue;
+
+  // Can push as long as there is space
+  for(int i = 0; i < 10; ++i)
   {
-    /// Nothing
+    EXPECT_TRUE(queue.push(i));
   }
 
-  nsg::CircularQueue<int, 10> m_queue;
-};
+  // Can't push while its full
+  EXPECT_FALSE(queue.push(10));
 
-
-TEST_F(CircularQueueTest, CanBePushedInto) 
-{
-  EXPECT_TRUE(m_queue.push(42));
-}
-
-TEST_F(CircularQueueTest, CanBePopFrom) 
-{
-  EXPECT_TRUE(m_queue.push(42));
-  
   int integer;
-  EXPECT_TRUE(m_queue.pop(integer));
+  // Can pop as long as there are values
+  for(int i = 0; i < 10; ++i)
+  {
+    EXPECT_TRUE(queue.pop(integer));
+  }
+
+  // Can't pop while its full
+  EXPECT_FALSE(queue.pop(integer));
+
+  // Can push with lambda
+  EXPECT_TRUE(queue.push(42, [](const int& src, int& dst)
+  {
+    dst = std::move(src);
+  }));
+
+  // Can pop with lambda
+  EXPECT_TRUE(queue.pop(integer, [](const int& src, int& dst)
+  {
+    dst = std::move(src);
+  }));
 
   EXPECT_EQ(integer, 42);
 }
